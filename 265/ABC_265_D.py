a4f8bb48916fe1b96f7f933c5db98c0deebf5c8b
maekawa <himahimastudent@gmail.com>
2022-09-06 11:14:27 +0900

    ABC practice

--- 265/ABC_265_D.py
+++ 265/ABC_265_D.py
@@ -1,66 +1,26 @@
-import sys
-
-N, P, Q, R = map(int,input().split())
-
+n, p, q, r = map(int,input().split())
 A = list(map(int,input().split()))
 
-total_sum = [0 for i in range(N)]
-total_sum[0] = A[0]
+y, z, w = 0, 0, 0
+s0,s1,s2 = 0, 0, 0
 
-for i in range(1, N):
-    total_sum[i] = total_sum[i-1] + A[i]
+ok = 0
+
+for x in range(n):
+    while y < n and s0 < p:
+        s0 += A[y]
+        s1 -= A[y]
+        y+= 1
+    while z < n and s1 < q:
+        s1 += A[z]
+        s2 -= A[z]
+        z+= 1
+    while w < n and s2 < r:
+        s2 += A[w]
+        w+= 1
+    if s0 == p and s1 == q and s2 == r:
+        ok = 1
+        break
+    s0 -= A[x]
+print("Yes" if ok else "No")
     
-"""
-for x in range(N-3):
-    for y in range(x+1, N-2):
-        total_sum_1 = total_sum[y-1] - total_sum[x-1]
-        if total_sum_1 != P:
-            continue
-        for z in range(y+1, N-1):
-            total_sum_2 = total_sum[z-1] - total_sum[y-1]
-            if total_sum_2 != Q:
-                continue
-            for w in range(z+1, N):
-                total_sum_3 = total_sum[w-1] - total_sum[z-1]
-                if total_sum_3 == R:
-                    print("Yes")
-                    sys.exit()
-"""
-y = N-3
-z = N-2
-w = N-1
-
-for x in range(N-3):
-    sum_1 = 0
-    while sum_1 != P and y > x:
-        sum_1 = total_sum[y-1] - total_sum[x-1]
-        if sum_1 > P:
-            y = (x+y)//2
-        elif sum_1 < P:
-            y = (y+N-3)//2
-    if sum_1 != P:
-        continue
-    sum_2 = 0
-    while sum_2 != Q and z > y:
-        sum_1 = total_sum[z-1] - total_sum[y-1]
-        if sum_2 > Q:
-            z = (y+z)//2
-        elif sum_2 < Q:
-            z = (z+N-2)//2
-    if sum_2 != Q:
-        continue
-    sum_3 = 0
-    while sum_3 != R and w > z:
-        sum_1 = total_sum[w-1] - total_sum[z-1]
-        if sum_3 > R:
-            w = (z+w)//2
-        elif sum_3 < R:
-            w = (z+N-1)//2
-    if sum_3 != R:
-        continue
-    else:
-        print("Yes")
-        sys.exit()
-        
-print("No")
-                
