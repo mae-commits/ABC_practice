9147c7292519a8e89ab63dacc416f52bc193ce2f
maekawa <himahimastudent@gmail.com>
2022-09-19 11:19:52 +0900

    233 269

---
+++ 233/ABC_233_D.py
@@ -0,0 +1,28 @@
+# 入力の受け取り
+N,K=map(int,input().split())
+A=list(map(int,input().split()))
+
+# Aの累積和
+A_cum=[0]*(N+1)
+
+# i=0~(N-1)まで
+for i in range(N):
+    # 累積和を計算
+    A_cum[i+1]=A_cum[i]+A[i]
+
+# defaultdictをインポート
+from collections import defaultdict
+# 連想配列
+X=defaultdict(int)
+# 答えをカウント
+ans=0
+
+# i=1~Nまで
+for i in range(1,N+1):
+    # A_cum[i-1]が増える
+    X[A_cum[i-1]]+=1
+    # A_cum[i]-Kとなるものをカウント
+    ans+=X[A_cum[i]-K]
+
+# 答えを出力
+print(ans)
--- 269/ABC_269_D.py
+++ 269/ABC_269_D.py
@@ -1,8 +1,71 @@
-N = int(input())
+# 入力の受け取り
+N=int(input())
 
-XY = list(list(map(int,input().split())) for i in range(N))
+# 黒いマスの管理
+Grid=[[0]*3000 for i in range(3000)]
 
-grid = [[-1,-1],[-1,0],[0,-1],[0,1],[1,0],[1,1]]
+# スタート地点の候補
+Start=[]
 
-ans = 0
+# N回
+for i in range(N):
+    # 入力の受け取り
+    X,Y=map(int,input().split())
+    # 0以上にするため1000プラスする
+    X+=1000
+    Y+=1000
+    # (X,Y)が黒いマス
+    Grid[X][Y]=1
+    # スタート地点の候補
+    Start.append([X,Y])
 
+# 訪問済み座標の管理
+visited=[[False]*3000 for i in range(3000)]
+
+# dequeを用意
+from collections import deque
+que=deque()
+
+# 答え
+ans=0
+
+# (1)Startから座標を取り出す
+for X,Y in Start:
+    # (2)取り出した座標が訪問済みでなければキューに入れて、訪問済みにする
+    if visited[X][Y]==0:
+        # キューへ追加
+        que.append([X,Y])
+        # 訪問済みにする
+        visited[X][Y]=1
+        # 答えをプラス1
+        ans+=1
+
+        # (4)キューが空になるまで
+        while 0<len(que):
+            # 今いる座標
+            NowX,NowY=que.popleft()
+
+            # ・今いる場所(キューから取り出した座標)から進める座標が黒いマス かつ 訪問済みでなければ
+            if Grid[NowX-1][NowY-1]==1 and visited[NowX-1][NowY-1]==0:
+                # (4_1)訪問済みにする
+                visited[NowX-1][NowY-1]=1
+                # (4_2)座標をキューに入れる
+                que.append([NowX-1,NowY-1])
+            if Grid[NowX-1][NowY]==1 and visited[NowX-1][NowY]==0:
+                visited[NowX-1][NowY]=1
+                que.append([NowX-1,NowY])
+            if Grid[NowX][NowY-1]==1 and visited[NowX][NowY-1]==0:
+                visited[NowX][NowY-1]=1
+                que.append([NowX,NowY-1])
+            if Grid[NowX][NowY+1]==1 and visited[NowX][NowY+1]==0:
+                visited[NowX][NowY+1]=1
+                que.append([NowX,NowY+1])
+            if Grid[NowX+1][NowY]==1 and visited[NowX+1][NowY]==0:
+                visited[NowX+1][NowY]=1
+                que.append([NowX+1,NowY])
+            if Grid[NowX+1][NowY+1]==1 and visited[NowX+1][NowY+1]==0:
+                visited[NowX+1][NowY+1]=1
+                que.append([NowX+1,NowY+1])
+
+# 答えの出力
+print(ans)
