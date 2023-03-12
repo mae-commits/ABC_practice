08fee82e42df68d244bdd1e7bb26c0f9bac33e21
maekawa <himahimastudent@gmail.com>
2023-03-12 11:19:00 +0900

    293

--- 293/ABC_293_C.py
+++ 293/ABC_293_C.py
@@ -6,32 +6,43 @@ a = list(list(map(int,input().split())) for i in range(h))
 
 count = 0
 
-arr = [True for i in range(h-1)]
-
-arr = arr + [False for i in range(w-1)]
-
-p = itertools.permutations(arr, h+w-2)
-
-route = set([a[0][0]])
-
-first_state = a[0][0]
-
-for v in p:
-    vertical = 0
-    horizontal = 0
-    route = set([first_state])
-    for verify in v:
-        if verify == True:
-            # print(a[vertical+1][horizontal])
-            vertical+=1
-            route.add(a[vertical][horizontal])
+# bit 全探索により全ての場合を考える
+# 0: 縦方向の移動
+# 1: 横方向の移動
+# 縦、横方向の移動がマス内に収まるように管理
+for bit in range(1 << (h+w-2)):
+    vertical,horizontal = 0, 0
+    route = set([a[0][0]])
+    for j in range(h+w-2):
+        if (bit & (1 << j)):
+            if horizontal < w-1:
+                horizontal+=1
+                route.add(a[vertical][horizontal])  
         else:
-            # print(a[vertical][horizontal+1])
-            horizontal+=1
-            route.add(a[vertical][horizontal])
-        # print(vertical, horizontal)
-    # print(route)
+            if vertical < h-1:
+                vertical+=1
+                route.add(a[vertical][horizontal])         
+        # print(vertical,horizontal)
+        # print(route)
     if len(route) == h+w-1:
         count+=1
 
 print(count)
+        
+# for v in p:
+#     vertical = 0
+#     horizontal = 0
+#     route = set([first_state])
+#     for verify in v:
+#         if verify == True:
+#             # print(a[vertical+1][horizontal])
+#             vertical+=1
+#             route.add(a[vertical][horizontal])
+#         else:
+#             # print(a[vertical][horizontal+1])
+#             horizontal+=1
+#             route.add(a[vertical][horizontal])
+#         # print(vertical, horizontal)
+#     # print("done")
+#     if len(route) == h+w-1:
+#         count+=1
