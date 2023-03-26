ddde39f7b8382f8f842d432b648a78241083a53b
maekawa <himahimastudent@gmail.com>
2023-03-26 23:50:15 +0900

    295_D on the way

---
+++ 295/ABC_295_D.py
@@ -0,0 +1,34 @@
+from collections import deque
+import numpy as np
+
+# 標準入力
+S = deque(list(input()))
+
+len_S = len(S)
+
+# 条件を満たす (l,r) の組の個数をカウント
+count = 0
+
+# 各位の数字の個数をカウント
+digit_number_count = np.array([[0 for i in range(10)]])
+
+previous_digit_number_count = [0 for i in range(10)]
+
+# 始めの位を指定し、l桁目までの数字の個数を記録
+for l in range(len_S):
+    lth_num = int(S.popleft())
+    previous_digit_number_count[lth_num]+=1
+    digit_number_count = np.append(digit_number_count, np.array([previous_digit_number_count]), axis=0)
+
+# 部分文字列の調査
+for l in range(1, len_S+1):
+    # 部分文字列の長さが偶数nである場合のみ
+    # 条件を満足する文字列かどうかを判断
+    for t in range(1,(len_S+1-l)//2 + 1):
+        r = l+2*t-1
+        # l~r文字目までの各数字の文字数を計算
+        dif = digit_number_count[r] - digit_number_count[l-1]
+        if sum(dif%2)==0:
+            count+=1
+
+print(count)
