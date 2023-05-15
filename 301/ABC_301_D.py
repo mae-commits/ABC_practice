544791f1c7ac3eef70756739d3688dab963a2105
maekawa <himahimastudent@gmail.com>
2023-05-15 11:47:45 +0900

    301 D revised

--- 301/ABC_301_D.py
+++ 301/ABC_301_D.py
@@ -1,51 +1,19 @@
-import math
-
-S = list(input())
+S = list(reversed(input()))
 
 N = int(input())
 
-# N をバイナリ変換
-N_bin = list(map(str, format(N, 'b')))
+# 文字列Sのバイナリ変換の値
+s = 0
 
-# 整数Nを2進数表示した際の桁数をカウント
-N_bin_digit = len(N_bin)
-
-if N_bin_digit > len(S):
-    S = ["0" for i in range(N_bin_digit-len(S))] + S
-else:
-    N_bin = ["0" for i in range(len(S)-N_bin_digit)] + N_bin
-
-# 各2進数について1桁目からの順番で逆にソート
-S.reverse()
-N_bin.reverse()
-
-S_new = S
-
-print(S)
-print(S_new)
-
-# S, N それぞれについて各桁を比較
-# '?' の場合は 'N' 似合うように変更
+# 文字列Sの中で`1`であるものだけバイナリ変換の値として加算
 for i in range(len(S)):
-    if S[i] == "?" and i >= N_bin_digit:
-        S[i] = N_bin[i]
-    elif S[i] == "?" and i < N_bin_digit:
-        S[i] = "1"
-    if S_new[i] == "?":
-        S_new[i] = "0"
-    
-S.reverse()
-S_new.reverse()
-
-changed_S = int("".join(S), 2)
-changed_S_new = int("".join(S_new), 2)
-
-print(S)
-print(S_new)
-
-if N >= changed_S:
-    print(changed_S)
-elif changed_S_new <= N:
-    print(changed_S_new)
-else:
+    s |= (S[i] == "1") << i
+# 表せる最小の値を超えてしまっている場合は-1
+if s > N:
     print(-1)
+# そうでない場合は、小さな桁から'?'を1に変換し、Nとの大小を比較
+else:
+    for i in reversed(range(len(S))):
+        if S[i] == "?" and (s | 1 << i) <= N:
+            s |= 1 << i
+    print(s)    
