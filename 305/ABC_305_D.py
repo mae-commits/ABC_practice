48a41ede549cab7cfb7928f82d1b44b07dde9fbc
maekawa <himahimastudent@gmail.com>
2023-06-11 14:54:58 +0900

    305 D

--- 305/ABC_305_D.py
+++ 305/ABC_305_D.py
@@ -4,17 +4,19 @@ N = int(input())
 
 A = list(map(int, input().split()))
 
-sleep = [0]
+sleep = [0]*N
 
-for i in range(N):
-    if i % 2 != 0:
-        sleep.append(A[i+1] - A[i])
+# 就寝・起床時間時点で何時間寝たかの累積和
+for i in range(1, N):
+    if i % 2 == 0:
+        sleep[i] = sleep[i-1] + A[i] - A[i-1]
     else:
-        sleep.append(0)
+        sleep[i] = sleep[i-1]
 
 Q = int(input())
 dif = []
 
+# 各クエリの処理
 for i in range(Q):
     l, r = map(int, input().split())
     r_index = bisect_left(A, r)
@@ -27,13 +29,8 @@ for i in range(Q):
     if A_l != l:
         l_index -= 1
     if l_index %2 == 1:
-        minus += l - A_l
+        minus += l - A[l_index]
     if r_index %2 == 1:
-        minus -= r - A_r
-    start = l_index if l_index %2 == 1 else l_index + 1
-    end = r_index if r_index %2 == 1 else r_index + 1
-    minute = sum(sleep[start:end+1]) - minus
-    dif.append(minute)
-
-for i in range(Q):
-    print(dif[i])
+        minus -= r - A[r_index]
+    minute = sleep[r_index] - sleep[l_index] - minus
+    print(minute)
