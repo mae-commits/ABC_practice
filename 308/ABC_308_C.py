ed8d13d0c9edeabe75a5c0ab40bf6da1658b8adb
Naoki Maekawa <naokimaekawa@Naokis-MacBook-Pro.local>
2023-07-02 00:20:18 +0900

    ABC C revised

--- 308/ABC_308_C.py
+++ 308/ABC_308_C.py
@@ -1,11 +1,13 @@
+from decimal import Decimal, getcontext
 
 N = int(input())
 coins = []
 for i in range(N):
     A, B = map(int, input().split())
-    coin = [i, float((A+B)/A)]
+    getcontext().prec = 20
+    coin = [i, Decimal(str(A))/Decimal(str(A+B))]
     coins.append(coin)
     
-coins = sorted(coins, key=lambda x:x[1])
+coins = sorted(coins, reverse=True, key=lambda x:x[1])
 
 print(*[coins[i][0]+1 for i in range(N)])
